420af75d45001e1dde21951e8ff0e8988ac7e618
Jordan Blount <jordantblount@gmail.com>
2021-05-27 02:46:21 -0400

    Added logic for creating new book elements in javascript and displaying in HTML. Change the navigation bar to contain right side nav items. Changed some of the CSS style names.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is a quick project that I put together to test arrays and objects as part of The Odin Project
 
 Also, I experimented with "localstorage" finally
+
+https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+
+https://sabe.io/tutorials/how-to-create-modal-popup-box
+
+https://www.w3schools.com/howto/howto_css_modals.asp
--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
-// REMOVE LATER
-let bookLibrary = [
-    new Book('test', 'test', 100, true),
-    new Book('test', 'test', 100, true),
-    new Book('test', 'test', 100, true),
-    new Book('test', 'test', 100, true),
-];
+let libraryGrid = document.getElementById("library");
+let addBook = document.getElementById("add-btn");
+let addBtnCont = document.getElementById("btn-container");
+let delBtns = document.querySelectorAll(".del");
+let readBtns = document.querySelectorAll(".read");
+
+let bookLibrary = [];
+
+updateLibrary();
+addBook.addEventListener("click", function() {
+    let rnd = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+    addBookToLibrary("lol", "nigas", rnd, true);
+});
+
 
 function Book(title, author, pages, hasRead) {
     this.title = title;
@@ -14,46 +21,174 @@ function Book(title, author, pages, hasRead) {
 
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ` 
-        + hasRead ? `has read.` : `not read yet.`;
+        + this.hasRead ? `already read.` : `not read yet.`;
     }
-}
 
-// let theHobbit = new Book('The Hobbit', "Jordan Blount", 295, false);
-function addBookToLibrary(title, author, pages, hasRead) {
-    if(title !== '' && author !== '' && pages !== '' && hasRead) {
-        bookLibrary.push(new Book(title, author, pages, hasRead));
-    }   
+    this.changeStatus = function() {
+        if(!this.hasRead) {
+            this.hasRead = true;
+        } else {
+            this.hasRead = false;
+        }
+    }
+
+    this.getReadStatus = function() {
+        return this.hasRead ? `Read` : `Has not read`;
+    }
 }
 
 function openNewBookForm() {
     // Opens up a form to enter in book info
 }
 
-function createNewBook() {
-    // Not sure if I want to pull this directly from textContent
-    // or add it into the properties for this function
-    let book;
-    let author;
-    let pages;
-    let hasRead;
-
-    addBookToLibrary(title, author, pages, hasRead);
-    updateLibrary();
+function addBookToLibrary(title, author, pages, hasRead) {
+    if(title !== '' && author !== '' && pages !== 0) {
+        let book = new Book(title, author, pages, hasRead);
+        bookLibrary.push(book);
+        addBookToGrid(book, bookLibrary.length - 1);
+    }
 }
 
-function removeBookFromLibrary(book) {
-    // Has to be a book object or something of the sort
-    bookLibrary.pop(book);
-    updateLibrary();
+function addBookToGrid(book, index) {
+    let card = document.createElement("div");   
+    card.className = "card";
+    card.setAttribute("data-index", index);
+    
+    let title = document.createElement("p"); 
+    title.className = "title";
+    title.innerHTML = book.title;
+
+    let author = document.createElement("p");
+    author.className = "author";
+    author.innerHTML = book.author;
+
+    let pages = document.createElement("p");
+    pages.className = "pages";
+    pages.innerHTML = book.pages;
+
+    let hasRead = document.createElement("p");
+    hasRead.className = "hasRead";
+    if(book.hasRead) {
+        hasRead.style.color = "green";
+        hasRead.innerHTML = "Read";
+    } else {
+        hasRead.style.color = "red";
+        hasRead.innerHTML = "Not read yet";
+    }
+
+    //Button div
+    let btnCont = document.createElement("div");
+    btnCont.className = "btns";
+
+    let delBtn = document.createElement("button");
+    delBtn.className = "del btn";
+    delBtn.textContent = "Delete";
+
+    delBtn.addEventListener('click', function() {
+        removeBookFromLibrary(index);
+    });
+
+    let readBtn = document.createElement("button");
+    readBtn.className = "read btn";
+    readBtn.textContent = "Read";
+
+    readBtn.addEventListener('click', function() {
+        book.changeStatus();
+        if(book.hasRead) {
+            hasRead.style.color = "green";
+            hasRead.innerHTML = "Read";
+        } else {
+            hasRead.style.color = "red";
+            hasRead.innerHTML = "Not read yet";
+        }
+    });
+
+    btnCont.appendChild(delBtn);
+    btnCont.appendChild(readBtn);
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(hasRead);
+    card.appendChild(btnCont);
+
+    libraryGrid.insertBefore(card, addBtnCont);    
 }
 
-function changeStatus(book) {
-    // Pull from textContent?
-
-    // Get current status
-    bookLibrary[book].hasRead;
+function removeBookFromLibrary(id) {
+    bookLibrary.pop(bookLibrary[id]);
+    libraryGrid.removeChild(document.querySelector(`[data-index="${id}"]`));
 }
 
+function findBook(id) {
+    return bookLibrary[id];
+}
+
+//
 function updateLibrary() {
-    //Create logic to add to add new book to grid here
+    bookLibrary.forEach(function(item, index) {
+        let card = document.createElement("div");   
+        card.className = "card";
+        card.setAttribute("data-index", index);
+        
+        let title = document.createElement("p"); 
+        title.className = "title";
+        title.innerHTML = item.title;
+
+        let author = document.createElement("p");
+        author.className = "author";
+        author.innerHTML = item.author;
+
+        let pages = document.createElement("p");
+        pages.className = "pages";
+        pages.innerHTML = item.pages;
+
+        let hasRead = document.createElement("p");
+        hasRead.className = "hasRead";
+        if(item.hasRead) {
+            hasRead.style.color = "green";
+            hasRead.innerHTML = "Read";
+        } else {
+            hasRead.style.color = "red";
+            hasRead.innerHTML = "Not read yet";
+        }
+
+        //Button div
+        let btnCont = document.createElement("div");
+        btnCont.className = "btns";
+
+        let delBtn = document.createElement("button");
+        delBtn.className = "del btn";
+        delBtn.textContent = "Delete";
+
+        delBtn.addEventListener('click', function() {
+            removeBookFromLibrary(index);
+        });
+
+        let readBtn = document.createElement("button");
+        readBtn.className = "read btn";
+        readBtn.textContent = "Read";
+
+        readBtn.addEventListener('click', function() {
+            item.changeStatus();
+            if(item.hasRead) {
+                hasRead.style.color = "green";
+                hasRead.innerHTML = "Read";
+            } else {
+                hasRead.style.color = "red";
+                hasRead.innerHTML = "Not read yet";
+            }
+        });
+
+        btnCont.appendChild(delBtn);
+        btnCont.appendChild(readBtn);
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(hasRead);
+        card.appendChild(btnCont);
+
+        libraryGrid.insertBefore(card, addBtnCont);
+    });
 }
